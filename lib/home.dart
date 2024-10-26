cda4f8ac71c393476bb3016f1ef673ba2141b2be
dr4k33 <sampigoreng63@gmail.com>
2024-10-26 02:49:37 +0000

    eaa

---
+++ lib/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece
+    // of paper on which the UI appears.
+    return Material(
+        child: Column(children: [
+      ListTile(
+          title: Text("Es Cendol"),
+          subtitle: Text('10/26/24'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://storage.jakarta-tourism.go.id/public/articles/lnd4fcd77WqaUN1WFwSHLlAi5t8vAT-metaZXMtY2VuZG9sLW1pbnVtYW4tc2VnYXItc2FhdC1idWthLXB1YXNhLTE4MDYwOGguanBn-.jpg"))),
+      ListTile(
+          title: Text("Es Lemon"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://asset.kompas.com/crops/ntMzZRUMKnFSzP_yG5A9XKzF8sg=/0x1417:3376x3668/750x500/data/photo/2022/04/26/62674990aa6ac.jpg"))),
+      ListTile(
+          title: Text("Es teh"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.liputan6.com%2Flifestyle%2Fread%2F5287657%2F12-macam-es-segar-dan-resepnya-untuk-musim-panas-legakan-dahaga&psig=AOvVaw1LswlIwrBsYAJQeC2lPXS8&ust=1729996580702000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJiWnpiQq4kDFQAAAAAdAAAAABAb"))),
+      ListTile(
+          title: Text("seger"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://asset-2.tstatic.net/jateng/foto/bank/images/franchise-es-teh-seger.jpg"))),
+      ListTile(
+          title: Text("seger"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://storage.jakarta-tourism.go.id/public/articles/lnd4fcd77WqaUN1WFwSHLlAi5t8vAT-metaZXMtY2VuZG9sLW1pbnVtYW4tc2VnYXItc2FhdC1idWthLXB1YXNhLTE4MDYwOGguanBn-.jpg"))),
+      ListTile(
+          title: Text("seger"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://storage.jakarta-tourism.go.id/public/articles/lnd4fcd77WqaUN1WFwSHLlAi5t8vAT-metaZXMtY2VuZG9sLW1pbnVtYW4tc2VnYXItc2FhdC1idWthLXB1YXNhLTE4MDYwOGguanBn-.jpg"))),
+      ListTile(
+          title: Text("seger"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://storage.jakarta-tourism.go.id/public/articles/lnd4fcd77WqaUN1WFwSHLlAi5t8vAT-metaZXMtY2VuZG9sLW1pbnVtYW4tc2VnYXItc2FhdC1idWthLXB1YXNhLTE4MDYwOGguanBn-.jpg"))),
+      ListTile(
+          title: Text("seger"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://storage.jakarta-tourism.go.id/public/articles/lnd4fcd77WqaUN1WFwSHLlAi5t8vAT-metaZXMtY2VuZG9sLW1pbnVtYW4tc2VnYXItc2FhdC1idWthLXB1YXNhLTE4MDYwOGguanBn-.jpg"))),
+      ListTile(
+          title: Text("seger"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://storage.jakarta-tourism.go.id/public/articles/lnd4fcd77WqaUN1WFwSHLlAi5t8vAT-metaZXMtY2VuZG9sLW1pbnVtYW4tc2VnYXItc2FhdC1idWthLXB1YXNhLTE4MDYwOGguanBn-.jpg"))),
+      ListTile(
+          title: Text("seger"),
+          subtitle: Text('0220'),
+          leading: Image(
+              image: NetworkImage(
+                  "https://storage.jakarta-tourism.go.id/public/articles/lnd4fcd77WqaUN1WFwSHLlAi5t8vAT-metaZXMtY2VuZG9sLW1pbnVtYW4tc2VnYXItc2FhdC1idWthLXB1YXNhLTE4MDYwOGguanBn-.jpg"))),
+    ]));
+  }
+}
